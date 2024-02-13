2590576aee0d72ad76380de80ed44bd2172197e2
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-02-13 23:46:33 +0100

    seconda milestone

--- code.js
+++ code.js
@@ -8,31 +8,52 @@ const dipendenti=
         posizione:"Founder & CEO",
         foto:"wayne-barnett-founder-ceo.jpg",
     },
+
     {
         nome:"Angela Caroll",
         posizione:"Chief Editor",
         foto:"angela-caroll-chief-editor.jpg",
     },
+
     {
         nome:"Walter Gordon",
         posizione:"Office Manager",
         foto:"walter-gordon-office-manager.jpg",
     },
+
     {
         nome:"Angela Lopez",
         posizione:"Social Media Manager",
         foto:"angela-lopez-social-media-manager.jpg",
     },
+
     {
         nome:"Scott Estrada",
         posizione:"Developer",
         foto:"scott-estrada-developer.jpg",
     },
+
     {
         nome:"Barbara Ramos",
         posizione:"Graphic Designer",
         foto:"barbara-ramos-graphic-designer.jpg",
     },
-    
+
 
 ]
+
+// console.log(dipendenti)
+for(let i=0 ; i<dipendenti.length ; i++)
+{
+    // facendo questo ci salviamo i valori che leggiamo con il primo ciclo
+    const dipendente = dipendenti[i];
+
+    // Quindi, in questo ciclo, key sarà "nome", "posizione" e "foto" per ogni oggetto dipendente nell'array.
+    // da ricordarsi proprieta ( chiave / valore)
+    for(let key in dipendente)
+    {
+       console.log(`${key} : ${dipendente[key]},`) 
+    //    in breve, stai utilizzando key per accedere dinamicamente alle proprietà di ciascun oggetto dipendente nell'array dipendenti durante la tua iterazione.
+    }
+    
+}
