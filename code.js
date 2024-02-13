25e37a57c20fa4aa894c8ec553fd3a4c3284cc88
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-02-13 23:20:57 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,37 @@
+// dichiarazione variabile
+const dipendenti=
+[
+    // dichiarazione dell'oggetto 1
+    {
+        // ricordarsi di mettere le ,
+        nome:"Wayne Barnett",
+        posizione:"Founder & CEO",
+        foto:"wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        nome:"Angela Caroll",
+        posizione:"Chief Editor",
+        foto:"angela-caroll-chief-editor.jpg",
+    },
+    {
+        nome:"Walter Gordon",
+        posizione:"Office Manager",
+        foto:"walter-gordon-office-manager.jpg",
+    },
+    {
+        nome:"Angela Lopez",
+        posizione:"Social Media Manager",
+        foto:"angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome:"Scott Estrada",
+        posizione:"Developer",
+        foto:"scott-estrada-developer.jpg",
+    },
+    {
+        nome:"Barbara Ramos",
+        posizione:"Graphic Designer",
+        foto:"barbara-ramos-graphic-designer.jpg",
+    }
+
+]
